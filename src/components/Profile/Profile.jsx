b0ec78e31f4c75d1684a00a4fab5abd15c68b9aa
Grootoportunities <tapir1337@mail.ru>
2023-08-28 16:50:39 +0300

    18. Dialogs Page
    Created dialogs page, add tags of profile and dialog in one div, to give them general style css

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,9 +3,12 @@ import modules from "./Profile.module.css";
 
 const Profile = () => {
   return (
-    <div className={modules.profile}>
+    <div>
       <div>
-        <img src="https://nystudio107.com/img/blog/_1200x675_crop_center-center_82_line/image_optimzation.jpg" className = {modules.bigPic}/>
+        <img
+          src="https://nystudio107.com/img/blog/_1200x675_crop_center-center_82_line/image_optimzation.jpg"
+          className={modules.bigPic}
+        />
       </div>
       <div>Ava + Description</div>
       <MyPosts />
