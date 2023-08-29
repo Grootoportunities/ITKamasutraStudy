3101bbf04e16bc3fc92ee4ab3157e87db543e6c3
Grootoportunities <tapir1337@mail.ru>
2023-08-29 17:01:58 +0300

    19. Route
    Learning how to change url between pages

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import { BrowserRouter, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <div className="app-wrapper-profile">
-        <Dialogs />
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-profile">
+          <Route component={Dialogs} />
+          <Route component={Profile} />
+        </div>
       </div>
-      {/* <Profile /> */}
-    </div>
+    </BrowserRouter>
   );
 };
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,8 +3,12 @@ import modules from "./Navbar.module.css";
 const Navbar = () => {
   return (
     <nav className={modules.nav}>
-      <div className={modules.item}>Profile</div>
-      <div className={modules.item}>Messages</div>
+      <div className={modules.item}>
+        <a href="/profile">Profile</a>
+      </div>
+      <div className={modules.item}>
+        <a href="/dialogs">Messages</a>
+      </div>
       <div className={modules.item}>News</div>
       <div className={modules.item}>Music</div>
       <div className={modules.item}>Settings</div>
