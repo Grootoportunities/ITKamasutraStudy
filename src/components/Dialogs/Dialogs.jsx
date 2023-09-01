d8b658b85cd28dd8f5560d064d97caf3db8cd558
Grootoportunities <tapir1337@mail.ru>
2023-09-01 15:42:58 +0300

    21. Layout of Dialogs
    Added layout of dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,23 @@
-import modules from "./Dialogs.module.css"
+import modules from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
- return(
-    <div>
-     Dialogs
+  return (
+    <div className={modules.dialogs}>
+      <div className={modules.dialogsItems}>
+        <div className={modules.dialog + ' ' + modules.active}>Daniil</div>
+        <div className={modules.dialog}>Anastasia</div>
+        <div className={modules.dialog}>Dmitriy</div>
+        <div className={modules.dialog}>Stanislav</div>
+        <div className={modules.dialog}>Nikita</div>
+        <div className={modules.dialog}>Maxim</div>
+      </div>
+      <div className={modules.messages}>
+        <div className={modules.message}>Hi</div>
+        <div className={modules.message}>How are you?</div>
+        <div className={modules.message}>Everything is fine!</div>
+      </div>
     </div>
-    );
+  );
 };
 
 export default Dialogs;
